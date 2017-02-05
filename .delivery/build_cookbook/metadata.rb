7ef60f00fb137ec7e01b135f26088321c087d21e
Austin Heiman <atheimanksu@gmail.com>
2017-02-05 07:06:17 -0600

    Add generated delivery build cookbook

---
+++ .delivery/build_cookbook/metadata.rb
@@ -0,0 +1,7 @@
+name 'build_cookbook'
+maintainer 'Austin Heiman'
+maintainer_email 'atheimanksu@gmail.com'
+license 'all_rights'
+version '0.1.0'
+
+depends 'delivery-truck'
---
+++ .delivery/build_cookbook/recipes/functional.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: build_cookbook
+# Recipe:: functional
+#
+# Copyright (c) 2017 Austin Heiman, All Rights Reserved.
+include_recipe 'delivery-truck::functional'
